7da12ba7c7e943572827d6a388851ecc3ac7edcf
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2024-05-16 08:08:39 +0200

    added navigation guards for the single todo page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import SignInView from '@/views/SignInView.vue'
+import SignUpView from '@/views/SignUpView.vue'
+import { useCurrentUser } from 'vuefire'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,19 +15,29 @@ const router = createRouter({
     {
       path: '/todos/edit/:id',
       name: 'todo',
-      component: () => import('@/views/TodoView.vue')
+      component: () => import('@/views/TodoView.vue'),
+      meta: { requiresAuth: true }
     },
     {
       path: '/sign-in',
       name: 'signIn',
-      component: () => import('@/views/SignInView.vue')
+      component: SignInView
     },
     {
       path: '/sign-up',
       name: 'signUp',
-      component: () => import('@/views/SignUpView.vue')
+      component: SignUpView
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const user = useCurrentUser()
+  if (to.matched.some((record) => record.meta.requiresAuth) && (!user.value || !user.value.email)) {
+    next({ name: 'signIn' })
+  } else {
+    next()
+  }
 })
 
 export default router
